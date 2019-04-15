cc2f6e1fd1d58bbaad087f6187aa22497858a7c0
Kathy Chen <kathychen.social@gmail.com>
2019-04-14 21:03:25 -0400

    work in progress

---
+++ app/data/friends.js
@@ -0,0 +1,28 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the friend profile.
+// Initially we just set it equal to a "dummy" friend.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var friendProfile = [
+    {
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendProfile;
