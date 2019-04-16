80329b846083dd4de13e279266d0feffe315036d
Kathy Chen <kathychen.social@gmail.com>
2019-04-15 23:29:32 -0400

    setup minimum requirement and fixed routing

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -16,11 +16,11 @@ module.exports = function(app) {
   // ---------------------------------------------------------------------------
 
   app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/survey"));
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
   // If no matching route is found default to home
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 };
